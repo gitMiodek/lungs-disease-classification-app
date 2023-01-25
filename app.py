ff8080d1d6d1d9a2efcea28446b8c6eafd147cd9
Jakub <kubamioduszewski@gmail.com>
2023-01-25 19:56:09 +0100

    feat(api): Added basic frontend layer so users have more friendly feeling

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from cnn import model
 from img_preprocess import img_transform
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 from PIL import Image
 import torch.nn.functional as F
 import torch
 
 app = FastAPI()
 
+@app.get("/")
+async def index():
+    html = """
+           <!DOCTYPE html>
+           <html>
+           <head>
+               <title>Upload Image</title>
+           </head>
+           <body>
+               <h1>Upload Image</h1>
+               <form method="post" enctype="multipart/form-data" action="/upload_image">
+                   <input type="file" name="img" id="img">
+                   <input type="submit" value="Upload">
+               </form>
+           </body>
+           </html>
+       """
+    return HTMLResponse(content=html)
 
 @app.post('/upload_image')
 async def predict_image(img: UploadFile = File(...)):
